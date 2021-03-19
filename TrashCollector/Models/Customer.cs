12700f31a5f46b7df6368d88f396d5002e650da4
ElijahCrane <chris.maychet.t1@gmail.com>
2021-03-19 15:20:44 -0500

    broke my website somehow

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -13,7 +13,25 @@ namespace TrashCollector
 	{
 		[Key]
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PickUpMonth { get; set; }
+		public string PickUpDay { get; set; }
+		public string PickUpYear { get; set; }
+		public string ExtraPickUpMonth { get; set; }
+		public string ExtraPickUpDay { get; set; }
+		public string ExtraPickUpYear { get; set; }
+		public string StartOfSuspensionMonth { get; set; }
+		public string StartOfSuspensionDay { get; set; }
+		public string StartOfSuspensionYear { get; set; }
+		public string EndOfSuspensionMonth { get; set; }
+		public string EndOfSuspensionDay { get; set; }
+		public string EndOfSuspensionYear { get; set; }
+		public string StreetName { get; set; }
+		public string State { get; set; }
+		public string ZipCode { get; set; }
+		
+
 		[ForeignKey("IdentityUser")]
 		public string IdentityUserId { get; set; }
 		public IdentityUser IdentityUser { get; set; }
